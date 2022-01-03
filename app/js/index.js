a75e54a619af15cf60667af2c07f4680572d43de
Ricardo-we <totgamer75@gmail.com>
2022-01-03 14:07:10 -0600

    first commit

---
+++ app/js/index.js
@@ -0,0 +1,28 @@
+const negativeButton = document.getElementById('no-button');
+const yesButton = document.getElementById('yes-button');
+const title = document.getElementById('title');
+const finalPart = document.getElementById('final');
+
+function changeButtonPosition(){
+    let moveLength = 400;
+    let randomX = Math.round(Math.random() * moveLength);
+    let randomY = Math.round(Math.random() * moveLength);
+    if(randomX < window.innerWidth && randomY < window.innerHeight){
+        negativeButton.style.top = Math.floor(randomY) - negativeButton.style.height + 'px';
+        negativeButton.style.left = Math.floor(randomX) - negativeButton.style.width + 'px';
+    }
+
+    console.log(window.getComputedStyle(negativeButton).marginTop);
+}
+document.addEventListener('DOMContentLoaded', () => {
+    
+    negativeButton.addEventListener('mouseenter', changeButtonPosition);
+    negativeButton.addEventListener('click', changeButtonPosition);
+    yesButton.addEventListener('click', () => {
+        negativeButton.classList.add('d-none');
+        yesButton.classList.add('d-none');
+        title.classList.add('d-none');
+        finalPart.classList.remove('d-none');        
+    })
+})
+    
